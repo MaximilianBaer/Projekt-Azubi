ea389c6926827385081a06d2b9c9f1283d2512bf
MaximilianBaer <maximilianteambaer@gmail.com>
2018-04-18 15:32:19 +0200

    Ende Calender

--- Calender/Calender/Program.cs
+++ Calender/Calender/Program.cs
@@ -17,7 +17,14 @@ namespace Calender
             string[] getrente_Eingabe = Eingabe_trennen(Eingabe);
             string[] neu_eingabe = sortierer(getrente_Eingabe);
             string[] befehl = ausfüllen(neu_eingabe);
-            string Ausgabe = calender(befehl, Prüfwert);
+            int anzahl = anzahl_tage(befehl);
+            int tage_index = tag_index(befehl);
+            int erstertag = erster_tag(befehl);
+            string[,] tag = tage();
+            int leer = leere_tage(erstertag, tage_index);
+            string[] tage_array = array_tag(leer, anzahl);
+
+            string Ausgabe = calender(befehl, Prüfwert,tage_index, tag, tage_array);
             Console.WriteLine(Ausgabe);
             goto start;
             
@@ -134,23 +141,148 @@ namespace Calender
             }
             return befehl;
         }
-        internal static string calender(string[] eingabe, bool Prüfwert)
+        internal static string[,] tage()
         {
-            
-            if (Prüfwert == false) { return "Befehl "+'"'+ eingabe[0] +'"'+" ist nicht vorhanden.";}
-            Console.WriteLine(eingabe);
+            string[,] tag_nr = new string[7,7] { 
+                { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" },
+                { "Di", "Mi", "Do", "Fr", "Sa", "So", "Mo" }, 
+                { "Mi", "Do", "Fr", "Sa", "So", "Mo", "Di" },
+                { "Do", "Fr", "Sa", "So", "Mo", "Di", "Mi" },
+                { "Fr", "Sa", "So", "Mo", "Di", "Mi", "Do" }, 
+                { "Sa", "So", "Mo", "Di", "Mi", "Do", "Fr" },
+                { "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" }
+            };
+            return tag_nr;
+        }
+        internal static int tag_index(string[] eigabe)
+        {
+            if (eigabe[3] == "Sonntag")
+            {
+                return 6;
+            }
+            else if (eigabe[3] == "Dienstag")
+            {
+                return 1;
+            }
+            else if (eigabe[3] == "Mittwoch")
+            {
+                return 2;
+            }
+            else if (eigabe[3] == "Donnerstag")
+            {
+                return 3;
+            }
+            else if (eigabe[3] == "Freitag")
+            {
+                return 4;
+            }
+            else if (eigabe[3] == "Samstag")
+            {
+                return 5;
+            }
+            else
+            {
+                return 0;
+            }
 
-            string[] tage = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
-            int tage_anzahl = DateTime.DaysInMonth(Convert.ToInt32(eingabe[1]), Convert.ToInt32(eingabe[2]));
-            DateTime erstr = new DateTime(Convert.ToInt32(eingabe[1]), Convert.ToInt32(eingabe[2]), 1);
-            //string Erster_tag = DateTime.;
-            
-            
+        }
+        internal static int anzahl_tage(string[] eingabe)
+        {
+            int tage_anzahl = DateTime.DaysInMonth(Convert.ToInt32(eingabe[2]), Convert.ToInt32(eingabe[1]));
+            return tage_anzahl;
+        }
+        internal static int erster_tag(string[] eingabe)
+        {
+            DateTime erster = new DateTime(Convert.ToInt32(eingabe[2]), Convert.ToInt32(eingabe[1]), 1);
+            string tag = Convert.ToString(erster.DayOfWeek);
+            int index_erster_tag;
+            if (tag == "Monday")
+            {
+                index_erster_tag = 0;
+            }
+            else if (tag == "Tuesday")
+            {
+                index_erster_tag = 1;
+            }
+            else if (tag == "Wednesday")
+            {
+                index_erster_tag = 2;
+            }
+            else if (tag == "Thursday")
+            {
+                index_erster_tag = 3;
+            }
+            else if (tag == "Friday")
+            {
+                index_erster_tag = 4;
+            }
+            else if (tag == "Saturday")
+            {
+                index_erster_tag = 5;
+            }
+            else 
+            {
+                index_erster_tag = 6;
+            }
+            return index_erster_tag;
+        }
+        internal static int leere_tage(int erster_tag, int tagindex )
+        {
+            int tage_verschieben = erster_tag - tagindex;
+            if (tage_verschieben < 0)
+            {
+                tage_verschieben = tage_verschieben + 7;
+            }
+            return tage_verschieben;
+        }
+        internal static string[] array_tag(int tage_verschieben, int anzahl_tage)
+        {
+            string[] tage_array = new string[anzahl_tage + tage_verschieben];
+            for (int i = 0; i < tage_verschieben; i++)
+            {
+                tage_array[i] = "  ";
+            }
+            for (int i = 0; i < anzahl_tage; i++)
+            {
+                tage_array[i + tage_verschieben] = Convert.ToString(i + 1);
+            }
+            for (int i = 0; i < tage_array.Length; i++)
+            {
+                if (tage_array[i].Length == 1)
+                {
+                    tage_array[i] = "0" + tage_array[i];
+                }
+            }
+            return tage_array;
+        }
+        internal static string calender(string[] eingabe, bool Prüfwert, int tagindex, string[,] tage, string[] tage_array)
+        {
+            if (Prüfwert == false) { return "Befehl " + '"' + eingabe[0] + '"' + " ist nicht vorhanden."; }
 
-
-
-
-            return "[Calender Einfügen]";
+            string reihe_1 = tage[tagindex,0]+" "+ tage[tagindex, 1] +" "+ tage[tagindex, 2] +" "+ tage[tagindex, 3] +" "+ tage[tagindex, 4] +" "+ tage[tagindex, 5] +" "+ tage[tagindex, 6];
+            string linie = "--------------------";
+            string reihe_2 = tage_array[0] + " " + tage_array[1] + " " + tage_array[2] + " " + tage_array[3] + " " + tage_array[4] + " " + tage_array[5] + " " + tage_array[6];
+            string reihe_3 = tage_array[7] + " " + tage_array[8] + " " + tage_array[9] + " " + tage_array[10] + " " + tage_array[11] + " " + tage_array[12] + " " + tage_array[13];
+            string reihe_4 = tage_array[14] + " " + tage_array[15] + " " + tage_array[16] + " " + tage_array[17] + " " + tage_array[18] + " " + tage_array[19] + " " + tage_array[20];
+            string reihe_5 = tage_array[21] + " " + tage_array[22] + " " + tage_array[23] + " " + tage_array[24] + " " + tage_array[25] + " " + tage_array[26] + " " + tage_array[27];
+            string reihe_6 = "";
+            for (int i = 0; i < tage_array.Length - 28 ; i++)
+            {
+                if (i <=6)
+                {
+                    reihe_6 = reihe_6 +tage_array[i+28] + " ";
+                }
+            }
+            string reihe_7 = "";
+            for (int i = 0; i < tage_array.Length - 35; i++)
+            {
+                if (i <= 6)
+                {
+                    reihe_7 = reihe_7 + tage_array[i + 35] + " ";
+                }
+            }
+            string ausgabe_ende = Environment.NewLine + reihe_1+ Environment.NewLine + linie + Environment.NewLine + reihe_2 + Environment.NewLine + reihe_3 + Environment.NewLine + reihe_4 + Environment.NewLine + reihe_5 + Environment.NewLine + reihe_6 + Environment.NewLine + reihe_7;
+            return ausgabe_ende;
         }
         
     }
